87321a388ce8c886d19ea961734ada1fbc0911d4
Aleksandar Beker <a.beker@vegait.rs>
2021-06-09 18:28:31 +0200

    registration approve/deny

--- src/app/pages/auth/registration/user-requests/user-requests.component.ts
+++ src/app/pages/auth/registration/user-requests/user-requests.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { NzMessageService } from 'ng-zorro-antd';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-user-requests',
@@ -7,9 +10,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserRequestsComponent implements OnInit {
 
-  constructor() { }
+  public listOfData = [];
+
+  constructor(private userService: UserService, 
+    private message: NzMessageService, 
+    private router: Router) { }
 
   ngOnInit(): void {
+    this.setupUsers();
+  }
+
+  private setupUsers(): void {
+    this.userService.getUserRegistrationRequests().subscribe(data => {
+      this.listOfData = data;
+    }, error => {
+      this.message.info(error.error.message);
+      this.router.navigateByUrl('dashboard');
+    });
+  }
+
+  public approve(id): void {
+    this.userService.approve(id).subscribe(data => {
+      this.message.success("Successfully approved.");
+    }, error => {
+      this.message.info(error.error.message);
+    });
+    location.reload();
+  }
+
+  public deny(id): void {
+    this.userService.deny(id).subscribe(data => {
+      this.message.success("Successfully denied.");
+    }, error => {
+      this.message.info(error.error.message);
+    });
+    location.reload();
   }
 
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -15,4 +15,16 @@ export class UserService {
   public registration(body): Observable<any> {
     return this.http.post(`${this.baseUrl}auth/registration`, body);
   }
+
+  public getUserRegistrationRequests(): Observable<any> {
+    return this.http.get(`${this.baseUrl}users/PENDING`);
+  }
+
+  public approve(id): Observable<any> {
+    return this.http.get(`${this.baseUrl}users/${id}/approve`);
+  }
+
+  public deny(id): Observable<any> {
+    return this.http.get(`${this.baseUrl}users/${id}/deny`);
+  }
 }
